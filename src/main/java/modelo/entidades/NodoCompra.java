771db02139d803f52c3f7737968a0a23b7ccc137
Oviedo-01 <estradaoviedol@gmail.com>
2025-11-13 23:17:13 -0500

    Estructura inicial del proyecto ProTienda y creacion de 4 clases

---
+++ src/main/java/modelo/entidades/NodoCompra.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo.entidades;
+
+/**
+ *
+ * @author estra
+ */
+public class NodoCompra {
+    public Compra compra;
+    public NodoCompra siguiente;
+    
+    public NodoCompra(Compra compra) {
+        this.compra = compra;
+        this.siguiente = null;
+    }
+}
